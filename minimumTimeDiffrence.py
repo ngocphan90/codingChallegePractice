f45f902e5ae4a8cf92ae83457bcd4c5aa8b66587
ngocphan90 <bichngoc0803@yahoo.com>
2021-11-21 15:44:36 -0600

    minimum time difference final

--- minimumTimeDiffrence.py
+++ minimumTimeDiffrence.py
@@ -14,8 +14,9 @@ def findMinDifference(timePoints):
     diff = float('inf')
     for i in range(len(timeList) - 1):
         diff = min(diff, timeList[i + 1] - timeList[i])
-    # add a day to 1st element and substract  and compare to the last
 
+
+    diff = min(diff, (1440 - timeList[len(timeList)-1] + timeList[0]))
 
     return diff
 
@@ -23,4 +24,4 @@ def findMinDifference(timePoints):
 
 if __name__ == '__main__':
 
-    print (findMinDifference(["23:59", "00:01", "02:00"]))
+    print(findMinDifference(["23:59", "00:01", "02:00"]))
