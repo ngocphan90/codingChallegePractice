cde96ae5ec0dfb60abcfcdfb9020ac55f2fe8856
ngocphan90 <bichngoc0803@yahoo.com>
2021-11-29 22:49:55 -0600

    detectStartOfLoopInLinkedList

---
+++ detectStartLoopOfLinkedList.py
@@ -0,0 +1,40 @@
+# The below function take head of Linked List as
+# input and return Address of first node in
+# the loop if present else return NULL.
+
+''' Definition for singly-linked list.
+* class ListNode:
+*	 def __init__(self, x):
+*		 self.val=x
+*		 self.next=None
+* '''
+
+
+def detectCycle(A):
+    # Declaring map to store node
+    # address
+    uset = set()
+
+    ptr = A
+
+    # Default consider that no cycle
+    # is present
+    while (ptr != None):
+
+        # Checking if address is already
+        # present in map
+        if (ptr in uset):
+            return ptr
+
+        # If address not present then
+        # insert into the set
+        else:
+            uset.add(ptr)
+
+    ptr = ptr.next
+
+
+    return None
+
+# This code is contributed by pratham76
+
