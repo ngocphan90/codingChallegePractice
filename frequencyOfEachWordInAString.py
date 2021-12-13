673acb94f46b3dc4d6047b0fa7c74424fe5baeba
ngocphan90 <bichngoc0803@yahoo.com>
2021-12-13 17:56:44 -0600

    freq each word

--- frequencyOfEachWordInAString.py
+++ frequencyOfEachWordInAString.py
@@ -47,3 +47,28 @@ def printFrequency(strr):
 # Driver Code
 strr = "Geeks For Geeks"
 printFrequency(strr)
+
+#second way
+
+
+def printFrequency(strr):
+
+    #define hashmap
+    hashMap ={}
+    #split the string
+    splitStr = strr.split()
+
+    #loop through the string
+    for i in range(0, len(splitStr)):
+        if splitStr[i] not in hashMap:
+            hashMap[splitStr[i]] = 1
+        else:
+            hashMap[splitStr[i]] += 1
+    for index in hashMap:
+        print(index, '_', hashMap[index])
+
+
+
+# Driver Code
+strr = "Geeks For Geeks"
+printFrequency(strr)
--- testing.py
+++ testing.py
@@ -1,8 +1,24 @@
-def fib(n):
-    memo = [-1]*20
-    if n <= 1:
-        return n
-    memo[n] = fib(n-1) + fib(n-2)
-    return memo[n]
+#Calculate the frequency of each word in the given string
 
-print(fib(4))
+
+def printFrequency(strr):
+
+    #define hashmap
+    hashMap ={}
+    #split the string
+    splitStr = strr.split()
+
+    #loop through the string
+    for i in range(0, len(splitStr)):
+        if splitStr[i] not in hashMap:
+            hashMap[splitStr[i]] = 1
+        else:
+            hashMap[splitStr[i]] += 1
+    for index in hashMap:
+        print(index, '_', hashMap[index])
+
+
+
+# Driver Code
+strr = "Geeks For Geeks"
+printFrequency(strr)
