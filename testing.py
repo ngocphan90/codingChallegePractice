f1f74972f16a0351137dab471daf5c888503df07
ngocphan90 <bichngoc0803@yahoo.com>
2021-12-13 19:00:31 -0600

    anagram

--- testing.py
+++ testing.py
@@ -1,15 +1,27 @@
-#removing duplicate characters from a string.
+# check if 2 strings are anagram
+#anagram if have the same character but order can be different
 
-def removeDuplicate(strr):
-    newList = []
-    for index in strr :
-        if index not in newList:
-            newList.append(index)
-    a = ''.join(newList)
-    return a
+def areAnagram(str1, str2):
+    if len(str1) != len(str2):
+        return False
+    st1= sorted(str1)
+    st2 = sorted(str2)
+
+    for i in range(0, len(st1)):
+        if st1[i] != st2[i]:
+            return False
+    return True
+
+
 
 
 
 # Driver code
-strr = "geeksforgeeks"
-print(removeDuplicate(strr))
+str1 = "cat"
+str2 = "act"
+# Function Call
+if areAnagram(str1, str2):
+    print("The two strings are anagram of each other")
+else:
+    print("The
+ two strings are not anagram of each other")
