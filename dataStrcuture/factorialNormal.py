121c900e907893bd415560906cfe079f07c7a515
ngocphan90 <bichngoc0803@yahoo.com>
2022-01-13 16:27:15 -0600

    recursive factorial

--- dataStrcuture/factorialNormal.py
+++ dataStrcuture/factorialNormal.py
@@ -1,15 +1,28 @@
 # 5! = 5X4X3X2X1
 # 2 approaches : interative or recursive
 
-# def iterative(number):
-    # answer = 1
-    # if number == 2:
-        # answer = 2
-    # for i in range (2, number+1):
-        # answer = answer * i
-    # return answer
+ # def iterative(number):
 
+    # array = [0, 1]
+    # for i in range (0, n+1):
+        # array.push(array[i-2] + array[i-1])
+    # return arr[n]
+
+    #  return array[number]
 
 
 
 # Python program to find the factorial of a number using recursion
+
+
+def iterative(number):
+    array = [0, 1]
+    for i in range(2, number + 1):
+        array.append(array[i - 2] + array[i - 1])
+    print(number)
+    print(array)
+    print(array[number])
+    return array[number]
+
+
+print(iterative(2))
--- dataStrcuture/factorialRecursive.py
+++ dataStrcuture/factorialRecursive.py
@@ -1,11 +1,10 @@
 
 def recursiveFactorial(number):
     # if number is 1 or 0 ---return 1
-    if number == 1 or number == 0:
-        # or if number < 2:
+    if number == 2:
         return number
     else:
-        return recursiveFactorial(number-1) + recursiveFactorial(number-2)
+        return number * recursiveFactorial(number-1)
 
 
-print(recursiveFactorial(8))
+print(recursiveFactorial(0))
