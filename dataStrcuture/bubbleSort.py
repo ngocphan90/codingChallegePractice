c7d33e81e6c0f0c84c8871924363695d623b3e49
ngocphan90 <bichngoc0803@yahoo.com>
2022-01-16 16:50:16 -0600

    bubbleSort final

--- dataStrcuture/bubbleSort.py
+++ dataStrcuture/bubbleSort.py
@@ -1,10 +1,11 @@
 def bubbleSort(array):
-    for i in range (0, len(array)):
-        for j in range (0, len(array)):
-            if array[i] > array [j]:
+    for i in range(0, len(array)):
+        for j in range(0, len(array)-1):
+            if array[j] > array[j+1]:
                 #swap number
                 temp = array[j]
                 array[j] = array[j+1]
                 array[j+1] = temp
+    return array
 
 print(bubbleSort([2,65,34,1,8]))
