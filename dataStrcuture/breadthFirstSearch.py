c216d68870563468df6bc6416f72ebda721a6d1e
ngocphan90 <bichngoc0803@yahoo.com>
2022-01-22 16:14:45 -0600

    bread first search recurisive

--- dataStrcuture/breadthFirstSearch.py
+++ dataStrcuture/breadthFirstSearch.py
@@ -32,10 +32,48 @@
 
 # return breadFirstSearch(queue, list)
 
-
-
-
-
-
-
 # function call breadFirstSearch(this.root, [])
+
+
+# DFSInorder()
+    # return traverseInorder(root,[])
+
+# traverseInorder(node, list)
+# if (node.left)
+#   traverseInorder(node.left, data)
+# list.append(node.value)
+
+# if (node.right)
+#   traverseInorder(node.right, data)
+
+# return list
+
+
+# DFSPreOrder()
+    # return traversePreorder(root,[])
+
+# traversePreorder(node, list)
+# list.append(node.value)
+# if (node.left)
+#   traverseInorder(node.left, data)
+
+
+# if (node.right)
+#   traverseInorder(node.right, data)
+
+# return list
+
+
+
+# DFSPostOrder()
+    # return traversePostOrder(root,[])
+
+# traversePostOrder(node, list)
+# if (node.left)
+#   traversePostOrder(node.left, data)
+
+# if (node.right)
+#   traversePostOrder(node.right, data)
+# list.append(node.value)
+
+# return list
