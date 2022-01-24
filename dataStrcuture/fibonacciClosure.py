ff91c04d159519c9b103615c03a7fa33365d0bab
ngocphan90 <bichngoc0803@yahoo.com>
2022-01-24 15:34:53 -0600

    closure fibonacci

---
+++ dataStrcuture/fibonacciClosure.py
@@ -0,0 +1,17 @@
+cache = {0: 0, 0: 1}
+
+
+def fibonacciMaster(n, cache):
+    if n in cache:
+        return cache[n]
+
+    else:
+        cache[n] = fibonacciMaster(n - 1, cache) + fibonacciMaster(n - 2, cache)
+        return cache[n]
+
+
+print(fibonacciMaster(4, cache))
+
+
+
+
